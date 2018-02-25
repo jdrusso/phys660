5822064934f2516dc9f5bfa7b2004cddcd887107
John Russo <jdr033@bucknell.edu>
2018-02-25 14:22:55 -0500

    More plot parameters added to increase font size and keep plots looking good.

--- project1/project1.hs
+++ project1/project1.hs
@@ -14,7 +14,9 @@ import Data.List
 -- Set some constants
 a_0 = 200.0 :: Double
 b_0 = 5.0 :: Double
-tA = 0.5 :: Double
+ra_0 = 150.0 :: Double
+rb_0 = 0.0 :: Double
+tA = 1 :: Double
 tB = 1 :: Double
 
 
@@ -27,24 +29,38 @@ a_pop t_max dt =
 -- Here, if track both the A and B populations, we can use the same iterative
 --  approach.
 pops t_max dt =
-  take (floor(t_max / dt)) $
+  take (floor(t_max / dt) + 1) $
   iterate (\n ->
     [n!!0 + n!!0*(-dt) ,
-    (n!!1 + (dt) * ((a_0/b_0)*n!!0 - (tA/tB)*n!!1))])
+    (n!!1 + (dt) * (n!!0 - (tA/tB)*n!!1))])
   [a_0, b_0]
+
+
+-- Here, we simulate the two populations where each type decays into the other.
+crossover t_max dt =
+  take (floor(t_max / dt) + 1) $
+  iterate (\n ->
+    [n!!0 + (0.5*n!!1 - n!!0)*(dt),
+    n!!1 + (n!!0 - 0.5*n!!1)*(dt)])
+  [ra_0, rb_0]
 
 main = do
 
   -- Set some generic attributes for the plots
-  let attrib = [XLabel "{/*1.75 Time (arbitrary units)}", YLabel "{/*1.75 Number of Atoms}"]
+  let attrib = [XLabel "{/*3 Time (arbitrary units)}", YLabel "{/*3 Number of Atoms}"]
 
   let t_max = 10.0
-  let dt = 0.01
+  let dt = 1
   let thepops = transpose $ pops t_max dt
+  -- let thepops = transpose $ crossover t_max dt
+
 
 
   plotPathsStyle (attrib ++ [EPS "logplot.eps",
-    Custom "logscale y; set key font \",18\"; set tics font \",18\";" []
+    Custom ("logscale y; set key font \",36\"; set tics font \",36\";" ++
+    "set ylabel offset -7; set xlabel offset 0,-1; set lmargin 16; set bmargin 5; set tmargin 2;"++
+    "set format y \"10^{%L}\"; set yrange [0.001:1000]; set ytics (1000,10,.1,.001);"++
+    "set xrange [0:10]; set xtics (0, 2, 4, 6, 8, 10)") []
     -- ,Title ("{/*2 Radioactive Atom Populations vs Time (tau_A: " ++ show tA ++ ", tau_B: " ++ show tB ++ ")}")
     ])$
 
@@ -60,28 +76,3 @@ main = do
     -- Zip a list of times in increments of dt to timestamp the population data
     [(defaultStyle{lineSpec=CustomStyle [LineTitle "N_A", LineWidth 10]}, zip [0,dt..] (thepops!!0)),
     (defaultStyle{lineSpec=CustomStyle [LineType 3, LineTitle "N_B", LineWidth 10]}, zip [0,dt..] (thepops!!1))]
-
-
-  -- -- Not very accurate
-  -- let t_max = 5
-  -- let dt = 0.75
-  -- plotPath (attrib ++ [PNG "inaccurate.png",
-  --   Title ("Inaccurate (Tmax: " ++ show t_max ++ ", dt: " ++ show dt ++ ")")])$
-  --   -- Zip a list of times in increments of dt to timestamp the population data
-  --   zip [0,dt..] (a_pop t_max dt)
-  --
-  -- -- Oscillatory
-  -- let t_max = 12
-  -- let dt = 1.5
-  -- plotPath (attrib ++ [PNG "oscillatory.png",
-  --   Title ("Oscillatory (Tmax: " ++ show t_max ++ ", dt: " ++ show dt ++ ")")])$
-  --   -- Zip a list of times in increments of dt to timestamp the population data
-  --   zip [0,dt..] (a_pop t_max dt)
-  --
-  -- -- Unstable
-  -- let t_max = 100
-  -- let dt = 5
-  -- plotPath (attrib ++ [PNG "unstable.png",
-  --   Title ("Unstable (Tmax: " ++ show t_max ++ ", dt: " ++ show dt ++ ")")])$
-  --   -- Zip a list of times in increments of dt to timestamp the population data
-  --   zip [0,dt..] (a_pop t_max dt)
