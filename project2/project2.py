7a2efe1b38fd3a1a56b0d75f933c3337432c5634
John Russo <jrusso@udel.edu>
2018-03-09 10:27:03 -0500

    Added next_event finder

--- project2/project2.py
+++ project2/project2.py
@@ -1,12 +1,13 @@
 import numpy as np
 
-x1 = 1.0
-x2 = 1.5
-v1 = -1.0
-v2 = -1.0
+END = -1
+COLLISION = 0
+BOUNCE = 1
 
-m1 = 1.
-m2 = 2.
+x1, x2 = 1.0, 1.5
+v1, v2 = -1.0, -1.0
+
+m1, m2 = 1., 2.
 
 tmax = 10
 dt = 0.1
@@ -14,82 +15,85 @@ steps = tmax/dt
 
 g = -1
 
-x = np.zeros([2,steps])
-v = np.zeros([2,steps])
+x = np.zeros([2,1])
+v = np.zeros([2,1])
+t = np.zeros([1])
+# Want to add new values with np.append(x, [[x1],[x2]], axis=1)
+
 
 x[0,0], x[1,0] = x1, x2
 v[0,0], v[1,0] = v1, v2
 
-Ei = 0.5*(m1/(m1+m2))*v1^2 + 0.5*(m2/(m1+m2))*v2^2 + m1/(m1+m2)*x1 + m2/(m1+m2)*x2
+Ei = 0.5*(m1/(m1+m2))*v1**2 + 0.5*(m2/(m1+m2))*v2**2 + m1/(m1+m2)*x1 + m2/(m1+m2)*x2
 
 eps = 2.2204E-16
 
 print("Initial energy is %f" % Ei)
 
 
-# Check if the first event will be a bounce or a collision
-# Time for the first ball to hit the ground
-bounce_time1 = [x for x in np.roots[0.5*g, v[0,0], x[0,0]] if x > 0][0]
-bounce_time2 = [x for x in np.roots[0.5*g, v[0,0], x[1,0]] if x > 0][0]
 
-# Check to see if the first event is a collision
-collide = bounce_time2 < bounce_time1
+# General algorithm:
+#   1. Determine whether next event will be a bounce or a collision.
+#   2. Determine the time, velocities and positions of that event.
+#   3. If time > t_max, then stop.
+#   4. Using the time, positions, and velocities of the balls at that event,
+#       calculate the time of the next event.
 
-# Find the collision time
-if collide:
-    event_time = (x[0,0]-x[1,0]) / (v[1,0]-v[0,0])
+# Find what the next event will be, and in how much time
+def next_event(xs, vs, t):
 
-#
-if not collide:
-    bounce = True
+    v1, v2 = vs[:,-1]
+    x1, x2 = xs[:,-1]
 
-collided = False
-bounced = False
-t == 0
-for i in range(steps):
+    # Do this by comparing bounce times, using the initial conditions from the
+    #   last elements in v and x.
+    bounce_time1 = [x for x in np.roots([0.5*g, v1, x1]) if x > 0][0]
+    bounce_time2 = [x for x in np.roots([0.5*g, v2, x2]) if x > 0][0]
 
-    # If a collision happened in the last step, reverse the direction of both balls
-    #   Use the new velocity and position to calculate whether it will bounce or
-    #   collide next.
-    if collided:
+    # If the second ball would bounce before the first, then a collision will occur
+    collide = bounce_time2 < bounce_time1
 
-        collided = False
-        bounce = True
-        pass
+    # Find the time of the event
+    event_time = (x1-x2) / (v2-v1) if collide else bounce_time1
 
-    # If a bounce happened in the last step, only reverse the direction of the bottom ball.
-    #   Also set its position to the absolute value of its position.
-    #   The next event WILL be a collision.
-    if bounced:
+    # Update v and x using kinematics equations
+    x1 += v1*event_time + 0.5 * g * event_time**2
+    v1 += g*event_time
 
-        bounced = False
-        collide = True
-        pass
+    x2 += v2*event_time + 0.5 * g * event_time**2
+    v2 += g*event_time
 
+    v1 *= (-1)
 
-    # If there's an impending collision, keep looping until it happens
+    # If it's a collision, also reverse the top velocity
     if collide:
+        v2 *= (-1)
 
-        # If the collision is going to happen withinin the next timestep, set
-        #   collided to True
-        continue
+    # If it's a bounce, set x1 to absolute value so it doesn't go through the
+    #   floor.
+    elif not collide:
+        x1 = abs(x1)
 
+    # Update the entries in the list
+    xs = np.append(xs, [[x1],[x2]], axis=1)
+    vs = np.append(vs, [[v1],[v2]], axis=1)
+    t = np.append(t, t[-1]+event_time)
 
-    # If there's an impending bounce, keep looping until it happens
-    elif bounce:
-
-        # If the bounce will happen within the next timestep, set bounced to
-        #   True.
-
-        continue
-
-    else:
-
-        pass
+    #  Return the updated lists
+    return xs, vs, t
 
 
+# Find all the events, record the times, positions, and velocities at them,
+#   and then interpolate functions between.
+while t[-1] <= tmax:
 
-
+    # Come up with the list of events
+    x, v, t = next_event(x, v, t)
+    print("Found event")
+    
+print(x)
+print(v)
+print(t)
 
 
 # for i in range(1,steps):
